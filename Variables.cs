c08752309d072620cc951628e80cea748899cc24
Aleksandr Sinyavtsev <sinyavtsev.alek@yandex.ru>
2020-08-28 01:35:23 +0300

    Declaration of variables

---
+++ Variables.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Class1
+{
+	public void main(string[] args)
+	{
+		byte byteVariable;
+		sbyte signedByteVariable;
+		short shortVariable;
+		ushort unsignedShortVariable;
+		int integerVariable;
+		uint unsignedIntegerVariable;
+		long longVariable;
+		ulong unsignedLongVariable;
+		char charVariable;
+		string stringVariable;
+		float floatVariable;
+		double doubleVariable;
+		bool booleanVariable;
+	}
+}
