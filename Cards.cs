3d24763f6e88e90927c94eae98d7ad8d6330091c
Aleksandr Sinyavtsev <sinyavtsev.alek@yandex.ru>
2020-08-28 02:53:32 +0300

    calculating the number of rows and the count of the remainder

---
+++ Cards.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Cards
+{
+	public void main(string[] args)
+	{
+		int countCards = 52;
+		int countRows = countCards / 3;
+		int remainsCards = countCards % 3;
+		Console.WriteLine(countRows);
+		Console.WriteLine(remainsCards);
+	}
+}
