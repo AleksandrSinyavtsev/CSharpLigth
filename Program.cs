b55d477f085139695420c9fb94a2a6e2f68a9fd8
Aleksandr Sinyavtsev <sinyavtsev.alek@yandex.ru>
2020-08-28 19:38:22 +0300

    Crystal store

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace crystal_store
+{
+    class CrystalStore
+    {
+        static void Main(string[] args)
+        {
+            int goldCount = 100;
+            int crystalPrice = 15;
+            int crystalCount = 5;
+                if (goldCount - crystalPrice * crystalCount >= 0)
+            {
+                 Console.WriteLine("Вы купили " + crystalCount + 
+                     " кристаллов, у вас осталось " + 
+                     (goldCount - crystalPrice * crystalCount) + " единиц золота!");
+            }
+                else
+            {
+                Console.WriteLine("У вас недостаточно золота для этой покупки");
+            }
+        }
+    }
+}
